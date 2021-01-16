1a6b42e7cb348aa17c40e2958340236cf6e83a15
iimandoo <euneundh@gmail.com>
2021-01-16 21:31:05 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
+
 const customers = [{
   'id' : 1,
   'image': 'https://placeimg.com/64/64/1',
@@ -22,24 +41,38 @@ const customers = [{
 
 class App extends Component {
   render(){
-      return (
-        <div>
-          {
-            customers.map(c => {
-              return(
-                <Customer 
-                  key= {c.key}
-                  name= {c.name}
-                  id= {c.id}
-                  image= {c.image}
-                  birthday= {c.birthday}
-                  />
-              );
-            })
-          }
-        </div>
-    );
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>생년월일</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+              customers.map(c => {
+                return(
+                  <Customer 
+                    key= {c.key}
+                    name= {c.name}
+                    id= {c.id}
+                    image= {c.image}
+                    birthday= {c.birthday}
+                    />
+                );
+              })
+            }
+            </TableBody>
+          </Table>
+        </Paper>
+    ); 
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
